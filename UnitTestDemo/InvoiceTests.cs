f47fa4bca3fe8afb33d7d8a289627025abc5fa70
Kelvin Wu <wufuhai@qne.com.my>
2022-09-28 10:41:29 +0800

    disposable

--- UnitTestDemo/InvoiceTests.cs
+++ UnitTestDemo/InvoiceTests.cs
@@ -1,25 +1,55 @@
 using FluentAssertions;
 using UnitTestDemo.Infrustracture;
+using Xunit.Abstractions;
 
 namespace UnitTestDemo
 {
-    public class InvoiceTests
+    public class InvoiceTests : IDisposable
     {
+        Invoice _inv = new Invoice();
+        private readonly ITestOutputHelper _output;
+
+        public InvoiceTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _output.WriteLine("Creating InvoiceTests class instance");
+        }
+
         [Fact]
         public void TestTaxCalculation()
         {
             // Arrange
-            var inv = new Invoice();
-            inv.Amount = 100;
-            inv.TaxRate = 0.10m;
-            inv.TaxInclusive = false;
+            _inv.Amount = 100;
+            _inv.TaxRate = 0.10m;
+            _inv.TaxInclusive = false;
 
             // Act
-            inv.Compute();
+            _inv.Compute();
 
             // Assert
             //Assert.Equal(110, inv.NetAmount);
-            inv.NetAmount.Should().Be(110m);
+            _inv.NetAmount.Should().Be(110m);
+        }
+
+        [Fact]
+        public void TestCancel()
+        {
+            // Arrange
+            _inv.Amount = 100;
+            _inv.TaxRate = 0.10m;
+            _inv.TaxInclusive = false;
+
+            // Act
+            _inv.Compute();
+
+            // Assert
+            //Assert.Equal(110, inv.NetAmount);
+            _inv.NetAmount.Should().Be(110m);
+        }
+
+        public void Dispose()
+        {
+            //_output.WriteLine("Disposing InvoiceTests class instance");
         }
     }
 }
