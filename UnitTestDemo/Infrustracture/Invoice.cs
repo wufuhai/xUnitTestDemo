b0da77da4207f41f560d9a25d89e3ed80a6fc9bb
KelvinWu <wufuhai@qne.com.my>
2022-09-28 09:50:02 +0800

    initial

---
+++ UnitTestDemo/Infrustracture/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace UnitTestDemo.Infrustracture
+{
+    public class Invoice
+    {
+        public Invoice()
+        {
+        }
+
+        public decimal Amount { get; set; }
+        public decimal TaxRate { get; set; }
+        public bool TaxInclusive { get; set; }
+        public decimal NetAmount { get; set; }
+
+        public bool IsCanceled { get; set; }
+
+        public void Compute()
+        {
+            NetAmount = TaxInclusive ? Amount : Amount * (1 + TaxRate);
+        }
+
+        public void Cancel()
+        {
+            IsCanceled = true;
+        }
+    }
+}
+
--- UnitTestDemo/InvoiceTests.cs
+++ UnitTestDemo/InvoiceTests.cs
@@ -1,11 +1,35 @@
+using UnitTestDemo.Infrustracture;
+
 namespace UnitTestDemo
 {
-    public class UnitTest1
+    public class InvoiceTests
     {
         [Fact]
-        public void Test1()
+        public void TestTaxCalculation()
         {
+            // Arrange
+            var inv = new Invoice();
+            inv.Amount = 100;
+            inv.TaxRate = 0.10m;
+            inv.TaxInclusive = false;
 
+            // Act
+            inv.Compute();
+
+            // Assert
+            Assert.Equal(110, inv.NetAmount);
+
+            // Re-Arrange
+            inv.TaxInclusive = true;
+            // Act
+            inv.Compute();
+
+            // Assert
+            Assert.Equal(100, inv.NetAmount);
+
+            // Testing different behavior in one test is a bad practice
+            //inv.Cancel();
+            //Assert.Equal(true, inv.IsCanceled);
         }
     }
 }
